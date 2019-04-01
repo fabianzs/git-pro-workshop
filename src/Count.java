423ef4f67004671edf81b2e1cc2ba0e3e5e9f7e9
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-01 14:46:42 +0200

    a3

--- src/Count.java
+++ src/Count.java
@@ -1,12 +1,8 @@
 public class Count {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        System.out.println("11");
-=======
+
     private int age;
 
     public void Count() {
         System.out.println(this.age);
->>>>>>> 8feb2c26ff869435b92ab4388f27c6b7c1f410e4
     }
 }
