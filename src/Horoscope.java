27237b2cc7fd8049c14cbb7d4eceb047d20dfc52
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-01 15:34:15 +0200

    a6

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -3,6 +3,7 @@ public class Horoscope {
     public void future(String horoscope, String param2){
         System.out.println(horoscope + param2);
         System.out.println("Laci");
-        System.out.println("mr lackland");
+
+
     }
 }
