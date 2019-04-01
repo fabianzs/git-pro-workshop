e3725a36c183868afa2888716cd54a5fe891c301
JohnLacklan <janos.f96@gmail.com>
2019-04-01 15:08:35 +0200

    Test

--- src/Greet.java
+++ src/Greet.java
@@ -6,7 +6,7 @@ public class Greet {
 
     public void writeName(String nameToPrint, String otherStringToPrint) {
         nameToPrint = "Molnár László";
-        //otherStringToPrint = "A nevem: ";
-       // System.out.println(otherStringToPrint + nameToPrint);
+        otherStringToPrint = "A nevem: ";
+        System.out.println(otherStringToPrint + nameToPrint);
     }
 }
--- src/Horoscope.java
+++ src/Horoscope.java
@@ -3,5 +3,6 @@ public class Horoscope {
     public void future(String horoscope, String param2){
         System.out.println(horoscope + param2);
         System.out.println("Laci");
+        System.out.println("megegy probakhddhhhhfhfhjfhfhhhfhhfhfhf");
     }
 }
