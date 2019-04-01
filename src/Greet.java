5c562ffd47b670fcc064e18806187e3027e562a7
Botond Balogh <botond.balogh8@gmail.com>
2019-04-01 15:07:22 +0200

    b6

--- src/Greet.java
+++ src/Greet.java
@@ -5,6 +5,6 @@ public class Greet {
     }
 
     public void writeName(String nameToPrint) {
-        System.out.println("Greetings!");
+        System.out.println("'Greetings for the " + nameToPrint);
     }
 }
