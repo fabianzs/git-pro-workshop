92caf0f8a03c70802791aaa7c94f3522c8138144
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-01 15:57:56 +0200

    Branch conflict fix

--- src/Greet.java
+++ src/Greet.java
@@ -4,25 +4,16 @@ public class Greet {
     public Greet() {
     }
 
-<<<<<<< HEAD
-    public void writeName(String nameToPrint, String otherStringToPrint) {
-        nameToPrint = "Molnár László";
-        otherStringToPrint = "A nevem: ";
-        System.out.println(otherStringToPrint + nameToPrint);
-=======
+
     public void writeName(String nameToPrint) {
         System.out.println("'Greetings for the " + nameToPrint);
->>>>>>> master
+
         System.out.println("another change");
         System.out.println("another change");
-        System.out.println("another change");
-        System.out.println("another change");
-<<<<<<< HEAD
-        System.out.println("another change  ");
-=======
         System.out.println("another change");
         System.out.println("another change");
 
->>>>>>> master
+        System.out.println("another change  ");
+
     }
 }
